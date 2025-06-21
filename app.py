62a6dd4adb58daeca70a03e0478f3d87e47ab25d
prakhar kaushik <24f1002537@ds.study.iitm.ac.in>
2025-06-21 12:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def get_outline():
 
 # To run the app locally, add:
 if __name__ == '__main__':
-     app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
